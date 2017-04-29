1effc8192e731dd3766400ec931a2cd66b5d63bd
LS1088 <ls496200691@gmail.com>
2017-04-29 20:24:34 +1200

    Update untitled-1.py

--- untitled-1.py
+++ untitled-1.py
@@ -1,4 +1,5 @@
 # Test
+import random
 
 def main():
     print ("hello world")
@@ -7,37 +8,98 @@ if __name__ == '__main__':
     None
 
 ##version 0.0.1
-version = "0.0.1"
-print("Project101, Version ", version)
+version = '0.0.1'
+author = 'pedololicon'
+print("Project101, \nVersion", version, "\nAuthor:", author)
 ##Trying to make a simple text-based adventure
 
 #Variables
 heroCurrentHp = 100
 heroMaxHp = 100
-heroWeapon = 'Fists'
+heroWeapon = 'Worn out sword'
 
 #Strings
-str1 = "Welcome to this simple text-based adventure"
-q1 = "Choose a name for your character"
-q2 = "You are awaken by the stench of rotten corpses. You don't remember anything. \nYou try to access the situation around you, and you find yourself in a dark cave."
+str1 = "\nWelcome to this simple text-based adventure"
+q1 = "\nChoose a name for your character"
+str2 = "\nYou are awaken by the stench of rotten corpses. You don't remember anything. \nYou try to access the situation around you, and you find yourself in a dark cave."
 
 #Functions
 def ask(question, *args, **kwargs):
     print (question)
-    if args:
-        print ("What is your next action?")
-    for num, ar in enumerate(args):
-        print (num+1, ar)
-    ans = input()
-    if ans == 'Check stats':
-        checkStats()
+    while True:
+        if args:
+            print ("What is your next action?")
+            for num, ar in enumerate(args):
+                print (num+1, ar)
+        ans = input()
+        if args:
+            if ans == 'explore' or '1':
+                explore()
+                break
+            elif ans == 'rest' or '2':
+                rest()
+                break
+            elif ans == 'check stats' or '3':
+                checkStats()
+        else:
+            break
     return ans
 
 def checkStats():
-    print (heroName, '\nHP: ', heroCurrentHp, '/', heroMaxHp, '\nCurrent weapon: ', heroWeapon)
+    print ('\nName: ', heroName, '\nHP: ', heroCurrentHp, '/', heroMaxHp, '\nCurrent weapon: ', heroWeapon, '\n')
+    
+def explore():
+    randomNumber = random.randrange(10000)
+    print('\ndice toss,', randomNumber)
+    if randomNumber < 5000:
+        combat()
+    elif randomNumber < 6000:
+        encounter()
+    elif randomNumber < 7000:
+        event()
+    elif randomNumber < 8000:
+        story()
+    else:
+        loot()
+    return
+    
+def rest():
+    randomNumber = random.randrange(10000)
+    print('\ndice toss,', randomNumber)
+    if randomNumber < 750:
+        combat()
+    elif randomNumber < 1250:
+        encounter()
+    else:
+        restHeal()
+    return
+
+def restHeal():
+    print('\nrestHeal function was called')
+    return
+
+def combat():
+    print('\ncombat function was called')
+    return
+
+def encounter():
+    print('\nencounter function was called')
+    return
+
+def event():
+    print('\nevent function was called')
+    return
+
+def story():
+    print('\nstory function was called')
+    return
+
+def loot():
+    print('\nloot function was called')
+    return
 
 #Start of I/O
 
 print (str1)
 heroName = ask(q1)
-ask(q2, 'Explore', 'Rest', 'Check stats')
+ask(str2, 'explore', 'rest', 'check stats')
