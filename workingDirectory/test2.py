5684f79b6824aba8d4d8c1b00f8cb7597ca6585d
Victor L <LS1088@users.noreply.github.com>
2017-05-19 15:49:59 +1200

    ffgddgfd

--- workingDirectory/test2.py
+++ workingDirectory/test2.py
@@ -1,13 +1,4 @@
-class testClass():
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
-    
-    def __str__(self):
-        return ("This is a test string.")
-    
-def testFunction(item):
-    print(item)
-    
-    
+import sys
+print(sys.path)
+
+import vlc
